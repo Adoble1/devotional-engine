f1b39c20aee46a65330b915405cebb43ba5dfa1b
Adoble1 <117782907+Adoble1@users.noreply.github.com>
2026-07-09 21:08:13 -0400

    Expose v6.4 blueprint-first engine as package default

--- devotional_engine/__init__.py
+++ devotional_engine/__init__.py
@@ -14,10 +14,19 @@ from .american_literature import (
 )
 from .approval import HumanApproval, require_human_approval
 from .audit import AuditRecord, build_audit_record, write_audit_record
+from .blueprint import (
+    BlueprintFinding,
+    StoryPlanBlueprint,
+    approve_blueprint,
+    build_blueprint,
+    validate_blueprint,
+    validate_script_alignment,
+)
 from .config import EngineConfig, ROLE_CONFIG
 from .classical_hymnody import CLASSICAL_HYMNODY_PROFILE, build_hymnody_brief
 from .poetic_music import POETIC_MUSIC_PROFILE, analyze_euphony, build_poetic_music_brief, has_midline_caesura
-from .engine import apply_american_literary_style, apply_editorial_smoothing, route_after_failure, run_engine
+from .engine import apply_american_literary_style, apply_editorial_smoothing, route_after_failure
+from .v64 import run_engine
 from .models import EngineContext
 from .renderer import render_flow_artifact
 from .stage_cycle import (
@@ -36,16 +45,18 @@ from .stage_cycle import (
 from .states import State
 
 __all__ = [
-    "AgentAdapter", "AuditRecord", "EngineConfig", "EngineContext", "ExternalModelAdapterConfig",
-    "HumanApproval", "MockAgentAdapter", "ROLE_CONFIG", "State", "UnconfiguredExternalModelAdapter",
-    "CLASSICAL_HYMNODY_PROFILE",
-    "POETIC_MUSIC_PROFILE",
-    "ReflectionReport", "RejectedArtifact", "StageArtifact", "StageName", "StageReport", "VerificationReport",
-    "AMERICAN_PATTERNS", "CREATIVE_MASTERY_LAYER", "AmericanLiteraryPattern", "StyleMatch",
-    "apply_american_literary_style", "apply_editorial_smoothing", "build_audit_record", "require_human_approval",
-    "analyze_euphony", "blocked_patterns", "build_hymnody_brief", "build_poetic_music_brief", "build_style_brief",
-    "has_midline_caesura", "match_american_patterns", "pre_1950_reference_patterns",
-    "planning_stage", "public_domain_patterns", "reflection_stage", "render_flow_artifact", "route_after_failure",
-    "run_engine", "run_stage_cycle", "tool_use_stage", "validate_external_adapter_config", "validate_style_brief",
+    "AgentAdapter", "AuditRecord", "BlueprintFinding", "EngineConfig", "EngineContext",
+    "ExternalModelAdapterConfig", "HumanApproval", "MockAgentAdapter", "ROLE_CONFIG", "State",
+    "StoryPlanBlueprint", "UnconfiguredExternalModelAdapter", "CLASSICAL_HYMNODY_PROFILE",
+    "POETIC_MUSIC_PROFILE", "ReflectionReport", "RejectedArtifact", "StageArtifact", "StageName",
+    "StageReport", "VerificationReport", "AMERICAN_PATTERNS", "CREATIVE_MASTERY_LAYER",
+    "AmericanLiteraryPattern", "StyleMatch", "apply_american_literary_style",
+    "apply_editorial_smoothing", "approve_blueprint", "blocked_patterns", "build_audit_record",
+    "build_blueprint", "require_human_approval", "analyze_euphony", "build_hymnody_brief",
+    "build_poetic_music_brief", "build_style_brief", "has_midline_caesura",
+    "match_american_patterns", "pre_1950_reference_patterns", "planning_stage",
+    "public_domain_patterns", "reflection_stage", "render_flow_artifact", "route_after_failure",
+    "run_engine", "run_stage_cycle", "tool_use_stage", "validate_blueprint",
+    "validate_external_adapter_config", "validate_script_alignment", "validate_style_brief",
     "verification_stage", "write_audit_record",
 ]
